213c47925939875ca26ca0141139342d4f75f24b
HalloTheEngineer <1q2lpz254@mozmail.com>
2026-01-13 15:31:17 +0100

    added a server-side opt-in for no-clip (required by Modrinth)

--- src/main/java/de/hallotheengineer/f5ranger/F5RangerClient.java
+++ src/main/java/de/hallotheengineer/f5ranger/F5RangerClient.java
@@ -6,17 +6,23 @@ import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class F5RangerClient implements ClientModInitializer {
     public static ModConfig config;
     public static final String MOD_ID = "f5ranger";
+    public static final Identifier ALLOW_NOCLIP_ID = new Identifier(MOD_ID, "allow_noclip");
 
     private static final float DEFAULT_VANILLA_DISTANCE = 4.0f;
 
     public static KeyBinding modifierKey;
+    public static boolean serverAllowsNoClip = false;
 
     @Override
     public void onInitializeClient() {
@@ -30,8 +36,14 @@ public class F5RangerClient implements ClientModInitializer {
         AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
         config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 
+        ClientPlayNetworking.registerGlobalReceiver(ALLOW_NOCLIP_ID, (client, handler, buf, responseSender) ->
+                client.execute(() -> serverAllowsNoClip = true));
+
         ClientLifecycleEvents.CLIENT_STOPPING.register(client ->
                 AutoConfig.getConfigHolder(ModConfig.class).save());
+
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) ->
+                serverAllowsNoClip = false);
     }
 
     public static void adjustDistance(double verticalAmount) {
@@ -48,5 +60,8 @@ public class F5RangerClient implements ClientModInitializer {
                 config.minDistance,
                 config.maxDistance
         );
+    }
+    public static boolean canNoClip() {
+        return config.noClip && (MinecraftClient.getInstance().isInSingleplayer() || serverAllowsNoClip);
     }
 }
